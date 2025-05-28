685ef28596086ff66ec645cb22c0f140bedbf09c
swetha <pswetha060@gmail.com>
2025-05-28 16:58:33 +0530

    meesahr

--- array.sh
+++ array.sh
@@ -2,7 +2,7 @@
 
 class=("a1" "a2" "a3" "a4")
 
-echo "first studenyt is $class[0]"
-echo "third rank is $class[2]"
-echo "first student is $class[3]"
-echo "last student is $class[5]"
+echo "first studenyt is ${class[0]}"
+echo "third rank is ${class[2]}"
+echo "first student is ${class[3]}"
+echo "last student is ${class[5]}"
