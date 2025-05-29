0fee8a9bf7acf33aa77221672a5d454843bcfe1b
swetha <pswetha060@gmail.com>
2025-05-29 17:00:45 +0530

    special variables

--- installpkg.sh
+++ installpkg.sh
@@ -5,12 +5,20 @@ user=$(id -u)
 if [ $user -eq 0 ]
 then 
    echo " user have root accesss:"
-   dnf install mysql -y
-if [ $? -ne 0 ]
-then 
-    echo "mysql is installed...hurray"
+dnf list installed mysql
+if  [ $? -ne 0 ]
+then
+     echo "my sql will intsall soon"
+    dnf install mysql -y
+    if [ $? -eq 0 ]
+    then 
+      echo "mysql is already intsalled - suucess"
+    else 
+      echo "mysql is already intsalled - fail"
+      exit 1
+    fi
 else 
-    echo "already installed"
+    echo "mysql is already intsalled, nothing to do ...!!"
 fi
 else 
     echo " user doesnt hjave root accesss:"
