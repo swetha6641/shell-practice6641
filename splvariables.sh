56bed8d449276cb5ae26a8a960214727813da043
swetha <pswetha060@gmail.com>
2025-05-29 15:59:02 +0530

    special variables

--- splvariables.sh
+++ splvariables.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "allvariables psssed to script:" $@
-echo "number of varaiables" $#
-echo "script name" $0
-echo "present working dir:" $pwd
-echo "ur home dir " $home
-echo "user name"$user
+echo "allvariables psssed to script:$@ "
+echo "number of varaiables $# "
+echo "script name $0"
+echo "present working dir: $pwd"
+echo "ur home dir  $home"
+echo "user name $user"
 sleep 10 &
-echo "PID of last cmd" $!
+echo "PID of last cmd $!"
+echo "PID of current $$"
 
 
 
