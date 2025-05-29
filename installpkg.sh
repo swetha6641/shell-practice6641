8058d348e465a81a2cefb6da5f8387ea42b90c9e
swetha <pswetha060@gmail.com>
2025-05-29 16:52:43 +0530

    special variables

--- installpkg.sh
+++ installpkg.sh
@@ -5,15 +5,22 @@ user=$(id -u)
 if [ $user -eq 0 ]
 then 
    echo " user have root accesss:"
-else 
-    echo " user doesnt hjave root accesss:"
-    exit 1
-fi
-dnf install mysql -y
-if [ $? -eq 0 ]
+   dnf install mysql -y
+if [ $? -ne 0 ]
 then 
     echo "mysql is installed...hurray"
 else 
     echo "already installed"
 fi
+else 
+    echo " user doesnt hjave root accesss:"
+    exit 1
+fi
+# dnf install mysql -y
+# if [ $? -eq 0 ]
+# then 
+#     echo "mysql is installed...hurray"
+# else 
+#     echo "already installed"
+# fi
 
