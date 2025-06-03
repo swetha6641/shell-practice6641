6e332c32d6176e16ef0d04a0c6e461118d3a22dc
swetha <pswetha060@gmail.com>
2025-06-03 11:56:34 +0530

    logs

---
+++ logs.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+user=$(id -u)
+Log_folder="/var/logs/shellop.log"
+script=$(echo $0 | cut "." -f1)
+log_file="$log_folde/$script.log"
+
+mkdir -p $log_folder
+echo "script started at :$(date)" &>>$log_file 
+if [ $user -ne 0 ]
+then  
+   echo -e "$R error: user have root accesss:$N" &>>$log_file  
+   exit 1
+else 
+   echo "you are running with root access" &>>$log_file 
+fi
+#chekcing the installled pkgs
+VALIDATE () {
+    if [ $1-eq 0 ]
+    then 
+      echo  -e "installing $2 is .. $G suucess $N" &>>$log_file  
+    else 
+      echo  -e "installing $2 is .. $R Failure $N" &>>$log_file 
+      exit 1
+    fi
+}
+dnf list installed mysql &>>$log_file 
+if  [ $? -ne 0 ]
+then
+    echo "my sql will install soon" &>>$log_file 
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else 
+    echo -e "mysql is already installed, $Y nothing to do ...!!$N" &>>$log_file 
+fi
+#installing nginx
+dnf list installed nginx &>>$log_file 
+if  [ $? -ne 0 ]
+then
+     echo "my nginx wil install soon" &>>$log_file 
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else 
+    echo "nginx alady installed, $Y nothing to do ...!!$N" &>>$log_file 
+fi
+# intsllaing python3
+dnf list installed python3 &>>$log_file 
+if  [ $? -ne 0 ]
+then
+     echo "my python3 wil install soon" &>>$log_file 
+    dnf install python3 -y &>>$log_file 
+    VALIDATE $? "python3"
+else 
+    echo "python3 alrrady instd, $N nothing to do ...$N!!" &>>$log_file 
+fi
